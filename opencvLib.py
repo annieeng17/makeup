01d0d8bcecec67c81a23eb21821e05bde9f77cf5
Annie Eng <annieeng2@gmail.com>
2020-04-08 00:47:36 -0400

    finally fixed lip, highlight and contour functions

--- opencvLib.py
+++ opencvLib.py
@@ -34,56 +34,61 @@ def find_cheeks (rects):
     
 def find_lips(rects):
     lip_ellipses = []
-
     for x1, y1, x2, y2 in rects:
-        delta = -(0.1*y2)
+        delta = -20   
         center_x1 = int((x1 + x2)/2) 
         center_y1 = int((0.9*y2) +(0.1*y1)) + delta
         axes_x1 = 25  
         axes_y1 = 12
-        lip_ellipses.append((center_x1,center_x1, axes_x1,axes_y1 ))
+        lip_ellipses.append((center_x1,center_y1, axes_x1,axes_y1 ))
     return lip_ellipses
 
+# algorithm used to reflect shape onto opposite side of face  
+# finding distance between x coordinate and midpoint, then doubling 
+# distance and adding that onto corresponding x coordinate
 def find_upper_cheeks(rects):
     upper_cheeks = []
     for x1, y1, x2, y2 in rects:
-        pt_1x = int(0.2*x1) + int(0.2*x2)
-        pt_1y = int(0.4*y1) + int(0.5*y2)
-        pt_2x = int(0.1*x1) + int(0.2*x2)
-        pt_2y = int(0.5*y1) + int(0.3*y2)
-        pt_3x = int(0.4*x1) + int(0.1*x2)
-        pt_3y = int(0.6*y1) + int(0.4*y2)
-        reflect_1x = -(pt_1x)
-        reflect_1y = -(pt_1y)
-        reflect_2x = -(pt_2x)
-        reflect_2y = -(pt_2y)
-        reflect_3x = -(pt_3x)
-        reflect_3y = -(pt_3y)
+        delta = -10
+        delta_2 = 5
+        pt_1x = int(0.2*x1) + int(0.8*x2) + delta_2
+        pt_1y = int(0.6*y1) + int(0.4*y2) + delta
+        pt_2x = int(0.1*x1) + int(0.9*x2) + delta_2
+        pt_2y = int(0.5*y1) + int(0.5*y2) + delta
+        pt_3x = int(0.3*x1) + int(0.7*x2) + delta_2
+        pt_3y = int(0.4*y1) + int(0.6*y2) + delta
+        midpoint = int((x1 + x2)/2)
+        distance_1 = midpoint - pt_1x
+        distance_2 = midpoint - pt_2x
+        distance_3 = midpoint - pt_3x
+        reflect_1x = 2*(distance_1) + (pt_1x)
+        reflect_2x = 2*(distance_2) + (pt_2x)
+        reflect_3x = 2*(distance_3) + (pt_3x)
         upper_cheeks.append((pt_1x,pt_1y, pt_2x,pt_2y , pt_3x,pt_3y))
-        upper_cheeks.append((reflect_1x,reflect_1y, reflect_2x,reflect_2y, \
-            reflect_3x,reflect_3y))
+        upper_cheeks.append((reflect_1x,pt_1y, reflect_2x,pt_2y,reflect_3x,pt_3y))
         
     return upper_cheeks
 
 def find_cheek_bones(rects):
     cheek_bones = []
     for x1, y1, x2, y2 in rects:
-        delta = 100
-        pt_1x = int(0.2*x1) + int(0.2*x2) + delta
-        pt_1y = int(0.4*y1) + int(0.5*y2) + delta
-        pt_2x = int(0.1*x1) + int(0.2*x2) + delta
-        pt_2y = int(0.5*y1) + int(0.3*y2) + delta
-        pt_3x = int(0.4*x1) + int(0.1*x2) + delta
-        pt_3y = int(0.6*y1) + int(0.4*y2) + delta
-        reflect_1x = -(pt_1x)
-        reflect_1y = -(pt_1y)
-        reflect_2x = -(pt_2x)
-        reflect_2y = -(pt_2y)
-        reflect_3x = -(pt_3x)
-        reflect_3y = -(pt_3y)
+        delta = 20
+        delta_2 = 5
+        pt_1x = int(0.1*x1) + int(0.9*x2) + delta_2
+        pt_1y = int(0.6*y1) + int(0.4*y2) + delta
+        pt_2x = int(0.1*x1) + int(0.9*x2) + delta_2
+        pt_2y = int(0.5*y1) + int(0.5*y2) + delta
+        pt_3x = int(0.3*x1) + int(0.7*x2) + delta_2
+        pt_3y = int(0.4*y1) + int(0.6*y2) + delta
+        midpoint = int((x1 + x2)/2)
+        distance_1 = midpoint - pt_1x
+        distance_2 = midpoint - pt_2x
+        distance_3 = midpoint - pt_3x
+        reflect_1x = 2*(distance_1) + (pt_1x)
+        reflect_2x = 2*(distance_2) + (pt_2x)
+        reflect_3x = 2*(distance_3) + (pt_3x)
         cheek_bones.append((pt_1x,pt_1y, pt_2x,pt_2y , pt_3x,pt_3y))
-        cheek_bones.append((reflect_1x,reflect_1y, reflect_2x,reflect_2y, \
-            reflect_3x,reflect_3y))
+        cheek_bones.append((reflect_1x,pt_1y, reflect_2x,pt_2y, reflect_3x,pt_3y))
     
     return cheek_bones
 
