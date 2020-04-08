04a52e9e1d049ec8ae3d995533bdedad68ac0374
Annie Eng <annieeng2@gmail.com>
2020-04-07 20:31:20 -0400

    modified upper cheek function

--- opencvLib.py
+++ opencvLib.py
@@ -36,8 +36,8 @@ def find_lips(rects):
     lip_ellipses = []
 
     for x1, y1, x2, y2 in rects:
-        delta = -200
-        center_x1 = int((x1 +x2)/2) 
+        delta = -(0.1*y2)
+        center_x1 = int((x1 + x2)/2) 
         center_y1 = int((0.9*y2) +(0.1*y1)) + delta
         axes_x1 = 25  
         axes_y1 = 12
@@ -47,7 +47,46 @@ def find_lips(rects):
 def find_upper_cheeks(rects):
     upper_cheeks = []
     for x1, y1, x2, y2 in rects:
+        pt_1x = int(0.2*x1) + int(0.2*x2)
+        pt_1y = int(0.4*y1) + int(0.5*y2)
+        pt_2x = int(0.1*x1) + int(0.2*x2)
+        pt_2y = int(0.5*y1) + int(0.3*y2)
+        pt_3x = int(0.4*x1) + int(0.1*x2)
+        pt_3y = int(0.6*y1) + int(0.4*y2)
+        reflect_1x = -(pt_1x)
+        reflect_1y = -(pt_1y)
+        reflect_2x = -(pt_2x)
+        reflect_2y = -(pt_2y)
+        reflect_3x = -(pt_3x)
+        reflect_3y = -(pt_3y)
+        upper_cheeks.append((pt_1x,pt_1y, pt_2x,pt_2y , pt_3x,pt_3y))
+        upper_cheeks.append((reflect_1x,reflect_1y, reflect_2x,reflect_2y, \
+            reflect_3x,reflect_3y))
         
+    return upper_cheeks
+
+def find_cheek_bones(rects):
+    cheek_bones = []
+    for x1, y1, x2, y2 in rects:
+        delta = 100
+        pt_1x = int(0.2*x1) + int(0.2*x2) + delta
+        pt_1y = int(0.4*y1) + int(0.5*y2) + delta
+        pt_2x = int(0.1*x1) + int(0.2*x2) + delta
+        pt_2y = int(0.5*y1) + int(0.3*y2) + delta
+        pt_3x = int(0.4*x1) + int(0.1*x2) + delta
+        pt_3y = int(0.6*y1) + int(0.4*y2) + delta
+        reflect_1x = -(pt_1x)
+        reflect_1y = -(pt_1y)
+        reflect_2x = -(pt_2x)
+        reflect_2y = -(pt_2y)
+        reflect_3x = -(pt_3x)
+        reflect_3y = -(pt_3y)
+        cheek_bones.append((pt_1x,pt_1y, pt_2x,pt_2y , pt_3x,pt_3y))
+        cheek_bones.append((reflect_1x,reflect_1y, reflect_2x,reflect_2y, \
+            reflect_3x,reflect_3y))
+    
+    return cheek_bones
+
 def find_avg_color(img, x, y):
     # turns into a numpy list, better than normal lists. can't do in python
     delta = 3
