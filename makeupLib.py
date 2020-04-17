4225f4aa02b72e762fdc87078b2323b39e48e8cc
Annie Eng <annieeng2@gmail.com>
2020-04-17 02:38:32 -0400

    new bgr colors for all skintones and undertone cheek pigments

--- makeupLib.py
+++ makeupLib.py
@@ -44,11 +44,11 @@ deep - (36,85,141)
 # if color is closest to tan then tan, 
 # if color is closest to deep then deep, 
 
-TONE_FAIR   = (226, 245, 253) 
-TONE_LIGHT  = (203, 241, 253)
-TONE_MEDIUM = (186, 229, 251)
-TONE_TAN    = (180, 220, 253)
-TONE_DEEP   = (255, 214, 164)
+TONE_FAIR   = (211, 231, 255) 
+TONE_LIGHT  = (185, 224, 255)
+TONE_MEDIUM = (142, 191, 239)
+TONE_TAN    = (120, 174, 237)
+TONE_DEEP   = ( 84, 134, 180)
 
 FEATURE_CHEEKS = 'CHEEKS'
 FEATURE_EYES   = 'EYES'
@@ -66,9 +66,21 @@ their information
 COLOR_PALETTE = {
     TONE_FAIR : {
         FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
+            UNDERTONE_COOL: (203, 191, 221),   #Light Pink
+            UNDERTONE_WARM: (168, 181, 249),   #Light Peach
+            UNDERTONE_NEUTRAL: (169, 172, 247) #Peachy Pink
+        },
+        FEATURE_EYES: {
+            UNDERTONE_COOL: (0, 0, 0),   #
+            UNDERTONE_WARM: (0, 0, 0),   #
+            UNDERTONE_NEUTRAL: (0, 0, 0) #
+        }
+    },
+    TONE_LIGHT : {
+        FEATURE_CHEEKS: {
+            UNDERTONE_COOL: (178, 162, 246),  #Baby Pink
+            UNDERTONE_WARM: (166, 181, 254),  #Peach
+            UNDERTONE_NEUTRAL: (148, 155, 218)#Sheer Plum
         },
         FEATURE_EYES: {
             UNDERTONE_COOL: (0, 0, 0),
@@ -76,11 +88,35 @@ COLOR_PALETTE = {
             UNDERTONE_NEUTRAL: (0, 0, 0)
         }
     },
-    TONE_LIGHT : {
+    TONE_MEDIUM : {
         FEATURE_CHEEKS: {
+            UNDERTONE_COOL: (175, 177, 211),#Mauve
+            UNDERTONE_WARM: (126, 141, 227),#Apricot
+            UNDERTONE_NEUTRAL: (96, 88, 195)#Soft Berry
+        },
+        FEATURE_EYES: {
             UNDERTONE_COOL: (0, 0, 0),
             UNDERTONE_WARM: (0, 0, 0),
             UNDERTONE_NEUTRAL: (0, 0, 0)
+        }
+    },
+    TONE_TAN : {
+        FEATURE_CHEEKS: {
+            UNDERTONE_COOL: (66, 42, 189),    #Rose
+            UNDERTONE_WARM: (124, 155, 248),  #Orangey Peach
+            UNDERTONE_NEUTRAL: (116, 151, 225)#Bronze
+        },
+        FEATURE_EYES: {
+            UNDERTONE_COOL: (0, 0, 0),
+            UNDERTONE_WARM: (0, 0, 0),
+            UNDERTONE_NEUTRAL: (0, 0, 0)
+        }
+    },
+    TONE_DEEP : {
+        FEATURE_CHEEKS: {
+            UNDERTONE_COOL: (88, 94, 159),  #Raisin
+            UNDERTONE_WARM: (49, 106, 245), #Bright Tangerine
+            UNDERTONE_NEUTRAL: (62, 66, 154)#Rich Cinnamon
         },
         FEATURE_EYES: {
             UNDERTONE_COOL: (0, 0, 0),
@@ -90,138 +126,4 @@ COLOR_PALETTE = {
     }
 }
 
-COLOR_PALETTE = {
-    TONE_FAIR : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    },
-    TONE_LIGHT : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    }
-}
-
-COLOR_PALETTE = {
-    TONE_FAIR : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    },
-    TONE_LIGHT : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    }
-}
-
-COLOR_PALETTE = {
-    TONE_FAIR : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    },
-    TONE_LIGHT : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    }
-}
-
-COLOR_PALETTE = {
-    TONE_FAIR : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    },
-    TONE_LIGHT : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    }
-}
-
-COLOR_PALETTE = {
-    TONE_FAIR : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    },
-    TONE_LIGHT : {
-        FEATURE_CHEEKS: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        },
-        FEATURE_EYES: {
-            UNDERTONE_COOL: (0, 0, 0),
-            UNDERTONE_WARM: (0, 0, 0),
-            UNDERTONE_NEUTRAL: (0, 0, 0)
-        }
-    }
-}
 # SKINTONES[TONE_FAIR][FEATURE_CHEEKS][UNDERTONE_COOL]
